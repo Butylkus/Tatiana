4daedb3eb886bb4a1caca13b32e1aa10ebf3b928
Butylkus <bugbut@mail.ru>
2016-04-21 01:35:18 +0300

    Исправлены опечатки, добавлен лог по умолчанию, перезапуски теперь логируются

--- 014-timecycle.py
+++ 014-timecycle.py
@@ -19,6 +19,10 @@ GPIO.output(18, 1)
 GPIO.output(22, 1)
 GPIO.output(27, 1)
 
+f = open("commonlog.txt", "a")
+f.write("Перезапуск скрипта: " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + " \n")
+f.close()
+
 #Считывает план и возвращает список. Вызывается по ходу выполнения в контексте "по ЭТОМУ плану" для функций device_off и device_on
 def planreader(plan):
     list = []
@@ -37,26 +41,26 @@ def planreader(plan):
 #Пин указывать номером согласно базовой схеме подключения. Если указать не тот, то неверное устройство будет работать по неверному плану.
 #Логфайл лучше всего именовать по имени девайса, например, LampaLog.txt.
 #В будущем лучше сделать единый лог без параметра функции, но уже сейчас можно писать в один файл, просто указав его для всех вызовов
-def device_off(pin, offplan, logfile):
+def device_off(pin, offplan, logfile="commonlog.txt"):
     for moment in planreader(offplan): #Дадада, я псих, рекурсия в наличии. Вызывает считыватель плана ещё раз на всякий пожарный
         if moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
             GPIO.output(pin,1)
             print("Выключено в ", moment) #при ручном запуске лучше раскомментировать, чтобы не смотреть в логи
             #Пишем в лог
             f = open(logfile, "a")
-            f.write("Устроство на пине " + str(pin) + " выключено в " + str(datetime.strftime(datetime.now(), "%H:%M:%S")) + "\n")
+            f.write("Устройство на пине " + str(pin) + " выключено в " + str(datetime.strftime(datetime.now(), "%H:%M:%S")) + "\n")
             f.close()
     
 #Функция Включения. Принимает пин, план и логфайл
 #полностью аналогична предыдущей, только включает, а не выключает.
-def device_on(pin, onplan, logfile):
+def device_on(pin, onplan, logfile="commonlog.txt"):
     for moment in planreader(onplan): #Дадада, я псих, рекурсия в наличии. Вызывает считыватель плана ещё раз на всякий пожарный
         if moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
             GPIO.output(pin,0)
             print("Включено в ", moment) #при ручном запуске лучше раскомментировать, чтобы не смотреть в логи
             #Пишем в лог
             f = open(logfile, "a")
-            f.write("Устроство на пине " + str(pin) + " включено в " + str(datetime.strftime(datetime.now(), "%H:%M:%S")) + "\n")
+            f.write("Устройство на пине " + str(pin) + " включено в " + str(datetime.strftime(datetime.now(), "%H:%M:%S")) + "\n")
             f.close()
 
 
