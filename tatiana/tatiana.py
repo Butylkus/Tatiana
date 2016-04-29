54278f446984cb60b91ec4f53049aec1bcb9a10b
butylkus <bugbut@mail.ru>
2016-04-29 05:38:39 +0300

    С нуля перепилена система планирования

--- tatiana/tatiana.py
+++ tatiana/tatiana.py
@@ -28,9 +28,11 @@ GPIO.setmode(GPIO.BCM) #Почему-то только в этом режиме
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 GPIO.output(17, 1)
 GPIO.output(18, 1)
 GPIO.output(27, 1)
+GPIO.output(22, 1)
 
 #ВХОДНЫЕ контакты (кнопки-выключатели)
 GPIO.setup(21, GPIO.IN) #тестовая кнопка
@@ -41,62 +43,57 @@ default_path = "/home/pi/.tatiana/"
 
 # ------------- ФУНКЦИИ ----------------
 
+# Функция планировщика. Вклюаети выключает устройства согласно заданному плану.
+# Формат плана:
+# PIN>ONTIME<OFFTIME\n
+# 10>чч:мм:сс<чч:мм:сс\n
+# %NEWLINE%
+# Файл обязательно должен заканчиваться строкой %NEWLINE% (без конечного \n) - это необходимо для работы веб-интерфейса!
+# Вообще лучше не трогать этот файл руками, а пользоваться только веб-интерфейсом.
 
-#Считывает план и возвращает список. Вызывается по ходу выполнения в контексте "по ЭТОМУ плану" для функции plan_switch()
-def planreader(plan):
-    list = []
-    f = open(plan, "r")
-    line = f.read()
-    list = line.split("\n")
-    f.close()
-    return list
-
-
-#Функция включения выключения по планам.
-#Принимает управляемый пин (устройство), план включения и план выключения.
-def plan_switch(pin_out, onplan, offplan, logfile=default_path + "commonlog.txt"):
-    pin_statusfile = default_path + "status/"+str(pin_out)
-    try:
-        f = open(pin_statusfile, "r")
-        f.close()
-    except FileNotFoundError:
-        f = open(pin_statusfile, "w")
-        status = f.write("0")
-        f.close()
-        status="1"
-    for moment in planreader(onplan): #Вызывает считыватель плана
-        if moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
-            #GPIO.output(pin_out,0) #Прямое включение. Конечный вариант - запись правильного статуса в правильный файл.
-            #Запись в статус-файл, из него значение уйдёт на управляющую функцию device()
-            f = open(pin_statusfile, "w")
+def plan(planfile=default_path + "plans/plan.txt", statusfile=default_path + "status/", logfile=default_path + "commonlog.txt"):
+    planfile = open(planfile, "r")
+    #print("Дескриптор:\n", planfile, "\n\n") #дебаг при ручном запуске
+    fullplan = planfile.read()
+    #print("Содержимое плана: \n", fullplan, "\n\n") #дебаг при ручном запуске
+    planstrings = fullplan.split("\n")
+    #print("Построчно:\n", planstrings, "\n\n") #дебаг при ручном запуске
+    index = len(planstrings)
+    
+        
+    for stringnumber in range(0,index-1):
+        pins = planstrings[stringnumber].split(">")
+        # Получаем время включения и выключения
+        times = pins[1].split("<")
+        pin = pins[0] #это номер пина
+        ontime = times[0] #Время включения
+        offtime = times[1] #Время выключения
+        moment = datetime.strftime(datetime.now(), "%H:%M:%S")
+        
+        #Проверяем время включения, пишем статус при совпадении, включаем девайс
+        if moment == ontime :
+            f = open(statusfile+pin, "w")
             f.write("0")
             f.close()
-            #print("Включено в ", moment) #при ручном запуске лучше раскомментировать, чтобы не смотреть в логи
-            #Пишем в лог
+            device(int(pin))
             f = open(logfile, "a")
-            f.write("Отметка плана для " + str(pin_out) + ", включение: " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
+            f.write("%PLANON% " + str(pin) + ">" + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
             f.close()
             while moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
                 continue
-    for moment in planreader(offplan): #Дадада, я псих, рекурсия в наличии. Вызывает считыватель плана ещё раз на всякий пожарный
-        if moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
-            #GPIO.output(pin_out,0) #Прямое включение. Конечный вариант - запись правильного статуса в правильный файл.
-            #Запись в статус-файл, из него значение уйдёт на управляющую функцию device()
-            f = open(pin_statusfile, "w")
+        if moment == offtime :
+            f = open(statusfile+pin, "w")
             f.write("1")
             f.close()
-            #print("Включено в ", moment) #при ручном запуске лучше раскомментировать, чтобы не смотреть в логи
-            #Пишем в лог
+            device(int(pin))
             f = open(logfile, "a")
-            f.write("Отметка плана для " + str(pin_out) + ", выключение: " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
+            f.write("%PLANOFF% " + str(pin) + ">" + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
             f.close()
             while moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
                 continue
-    device(pin_out,logfile)
-
 
 #Главная функция. Включает и отключает согласно статусу из соответствующего пину файла
-def device(pin, logfile=default_path + "commonlog.txt"):
+def device(pin):
     f_reader = default_path + "status/"+str(pin) #Цепляем правильный файл
     f = open(f_reader,"r") #читаем статус-файл пина
     status = int(f.read())
@@ -109,11 +106,7 @@ def device(pin, logfile=default_path + "commonlog.txt"):
 #Если кнопка нажата и реле выключено, то включить. Если нажата и реле включено - отключить
 def button(pin_in, pin_out, logfile=default_path + "commonlog.txt"):
     if GPIO.input(pin_in) == False:
-        #Логируем нажатие
         f = ""
-        f = open(logfile, "a")
-        f.write("Кнопка на пине " + str(pin_in) + " нажата " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
-        f.close()
         #Сверяем и переключаем статус выходного устройства
         path = default_path + "status/"+str(pin_out)
         try:
@@ -131,19 +124,30 @@ def button(pin_in, pin_out, logfile=default_path + "commonlog.txt"):
             f.close()
         #print (status) #Дебаг, отлов нажатия
         f = open(path, "w")
-        stfile = open(logfile, "a")
+        lfile = open(logfile, "a")
         if status == "0":
             f.write("1")
-            stfile.write("Устройство " + str(pin_out) + " выключено в " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
+            lfile.write("%BUTTONOFF% " + str(pin_in) + " > " + str(pin_out) + " > " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
         if status == "1":
             f.write("0")
-            stfile.write("Устройство " + str(pin_out) + " включено в " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
-        stfile.close()
+            lfile.write("%BUTTONON% " + str(pin_in) + " > " + str(pin_out) + " > " + str(datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")) + "\n")
+        lfile.close()
         f.close()
         device(pin_out)
         moment = datetime.strftime(datetime.now(), "%H:%M:%S")
         while moment == datetime.strftime(datetime.now(), "%H:%M:%S"):
             continue
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
 
 # ------------- ИСПОЛНЕНИЕ ----------------
 
@@ -156,13 +160,18 @@ f.close()
 while True:
     time.sleep(0.1) #Слегка снижает нагрузку на процессор, сокращая активность до 9-10 проходов в секунду
     
-    #всё наглядно: устройства на пине 18 включается по 18onplan.txt, выключается по 18offplan.txt, а сообщения выводит в общий лог.
-    #Для каждого нового устройства просто продублируйте вызов функции с новыми параметрами
-    plan_switch(18, default_path + "plans/18onplan.txt", default_path + "plans/18offplan.txt", logfile=default_path + "commonlog.txt")
+    #Проверяем файл плана и если время для какой-либо операции пришло, функция выполнит нужную операцию
+    plan()
     
-    #Для каждого нового устройства просто продублируйте вызов функции с новыми параметрами
-    button(21, 27, default_path + "commonlog.txt") #По сигналу 21 пина управляется устройство на 27
-
+    #Привязываем кнопку к устройству. Одна кнопка может управлять любыми устройствами. И наоборот, любое устройство может управляться любой кнопкой.
+    #КАЖДАЯ кнопка должна быть привязана к устройству, для этого просто дублируем функцию с нужными параметрами
+    button(21, 27, default_path + "commonlog.txt") #По сигналу кнопки (пин 21) управляется устройство (27 пин), пишется лог работы кнопки
+    
+    #Активация веб-интерфейса. Дублируем для каждого устройства, управляемого через веб-интерфейс
+    device(17)
+    device(18)
+    device(22)
+    device(27)
 
 
 # ------------- ВЫХОД ----------------
