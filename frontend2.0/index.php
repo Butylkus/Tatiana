02c32e0720398c2e501016e3a6a1394dc059f7db
Alexey Butylkus <bugbut@mail.ru>
2016-12-25 09:00:38 +0300

    Прикручена авторизация
    Авторизация проходит через сесси и куки. 
    ТуДу: Проверить настройки ПХП на живучесть сессий...

--- frontend2.0/index.php
+++ frontend2.0/index.php
@@ -1,35 +1,11 @@
 <?php
+ session_start();
+ if ($_SESSION['authorizedsids'][$_COOKIE['sid']] != "authorised"){
+     header("Location: http://".$_SERVER['HTTP_HOST']."/auth.php");
+     exit();
+ }
 
 include_once('settings.php');
-
-if ($_POST['lgn']==$login and $_POST['pwd']==$password){
-    $hash=$_SERVER['REMOTE_ADDR'].$login.$_SERVER['REMOTE_ADDR'].$password.date('sdsHsms').$_SERVER['REMOTE_ADDR'];
-    $hash = md5($hash);
-    setcookie("authcontrol",$hash,time()+600);
-    $sessfile = fopen("sess.txt","w+");
-    fwrite($sessfile,$hash);
-    fclose($sessfile);
-echo <<<RENEW
-<html>
-<head>
-<meta http-equiv="refresh" content="3"/>
-</head>
-<body><center>Привет, одну секунду...</center></body>
-</html>
-RENEW;
-exit();
-}
-$istherecookie = file_get_contents("sess.txt");
-if ($istherecookie == $_COOKIE["authcontrol"]){
-    $status = true;
-
-    }else{
-    $status = false;
-    unset($_COOKIE["authcontrol"]);    
-    }
-
-if ($status==true) {
-
 
 function show_plan($plan){
     #$echer=file_get_contents($plan);
@@ -52,15 +28,16 @@ function pintoname($pin_name,$plan){
     return $plan;
 }
 
+function logout(){
+$logout = "http://".$_SERVER['HTTP_HOST']."/auth.php?action=logout";
+return $logout;
+}
+
+
 
 $echoer = file_get_contents("template.html");
 $echoer = str_replace("%PLAN%",show_plan(pintoname($pin_name,$planfile)),$echoer);
-echo $echoer;
-}
-    
-else {
-$echoer = file_get_contents("form");
+$echoer = str_replace("%LOGOUT%",logout(),$echoer);
 echo $echoer;
 
-}
 ?>
