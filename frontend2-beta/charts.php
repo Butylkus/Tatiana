5cb17bfe73dfaba4b276fe8ddc800a84e85b35f2
Alexey Butylkus <bugbut@mail.ru>
2017-10-01 03:25:26 +0300

    Приближаем ввод графиков
    Убраны устаревшие файлы; Причёсан и немного оптимизирован базовый код; Запрос в обработчик графиков более логичен; JS-код запроса и вывода графиков финализирован (теперь только косметика)

---
+++ frontend2-beta/charts.php
@@ -0,0 +1,111 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Всяческие графики</title>
+    <style>
+        .svg-container {
+            font-size: 1.6em;
+            font-family: MONOSPACE;
+            text-align: center;
+           /* transform: scale(0.5); */
+        }
+        svg {
+            font-weight: bold;
+        }    
+        .xtime {
+            font-size: 0.75em;
+            font-weight: normal;
+        }
+        </style>
+  </head>
+  <body>
+  
+
+        <div class="svg-container">
+
+        
+       <?php
+include_once 'sys/settings.php';
+
+//Подключение к базе данных
+//ну так, на всякий случай
+
+
+///////////////////////////////////////////
+/////////////форма всей статы//////////////
+///////////////////////////////////////////
+
+
+
+// Генерируем форму для показа радиокнопок
+$form = 'График '."\n";
+//выводим селекты температуры и влажности
+$form .= "\t" . '<span class="typeselector"><select id="type"><option value="temp">температуры</option><option value="hmdt">влажности</option></select>'."\n";
+
+//запиливаем радиобатоны с пинами...
+//ищем пины в главной таблице
+$data = mysql_query("SELECT pin, name FROM pins WHERE direction='dht';");
+
+while ($row = mysql_fetch_assoc($data)) {
+    $pinname[$row["pin"]]=$row["name"]; //pin[№]=="имя"...
+}
+
+$form .= '<select id="pins">';
+foreach ($pinname as $key => $value){
+    $form .= "\t" . '<option value="'.$key.'">'.$value.'</option>'."\n";
+}
+$form .= "</select>\n";
+
+
+$form .= ' за <select id="period"><option value="day">сутки</option><option value="week">неделю</option><option value="month">месяц</option><option value="year">год</option></select>';
+
+//заканчиваем форму
+$form .= "\n\t<p><button onclick=\"btn()\">Показать</button><p>\n";
+
+echo $form;
+//exit(0);
+?>
+        
+        
+<!-- начало вставки --->        
+        <span id="chart-container"></span>
+        
+<!-- конец -->        
+
+        </div>
+        
+        
+        
+        
+<script>
+  function loadChart(holder) {
+    xmlhttp = new XMLHttpRequest();
+    var spanid = "chart-container";
+    var element = document.getElementById(spanid);
+    element.innerHTML = 'гружусь...';
+    xmlhttp.open("GET", holder);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            element.innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.send(null);
+    }
+	function btn(qstring){
+		var qstring ='';
+        var element = document.getElementById("type");
+		qstring = qstring + '?type=' + element.options[element.selectedIndex].value;
+		element = document.getElementById("pins");
+		qstring = qstring + '&pin=' + element.options[element.selectedIndex].value;
+		element = document.getElementById("period");
+		qstring = qstring + '&period=' + element.options[element.selectedIndex].value;
+		var element = document.getElementById("chart-container");
+        element.innerHTML = '<br><br>FOO>>> ' + qstring +' <<< BAR';
+			
+			loadChart("/sys/charts.php" + qstring);
+    }
+</script>
+
+  </body>
+</html>
+
--- frontend2-beta/sys/charts.php
+++ frontend2-beta/sys/charts.php
@@ -2,23 +2,36 @@
 
 header("Content-Type:text/txt; charset=UTF-8");
 
-//Подключение к базе данных
-$db = mysqli_connect("localhost","tatiana","tatiana","tatiana") or die("Не могу подключится к серверу БД");
-//ну так, на всякий случай
+include_once 'settings.php';
+
 $polypoints = "";
 $houraxis = "";
 $dayaxis = "";
 
-    
+// Ну а чего бы не повыпендриваться? Фильтруем шлак из пинов, ну и защита +2
+if (!preg_match('/^[1-9]$|^[1,2][0-7]$/', $_GET['pin']) or !is_numeric($_GET['pin'])){
+     exit("FUCK OFF!");
+}
+
+///////////////////////////////////////////
+/////////////ГОД И МЕСЯЦ///////////////////
+///////////////////////////////////////////
+
+if ($_GET['period']=="month" or $_GET['period']=="year"){
+    exit("<p>Пардон, пока в разработке ;)</p>");
+}
+
+
 ///////////////////////////////////////////
 /////////////СУТОЧНАЯ ТЕМПЕРАТУРА//////////
 ///////////////////////////////////////////
-if ($_GET['type']=="temp24"){
-    $data = mysqli_query($db, "SELECT temperature, timestamp FROM dht_data WHERE pin=3 ORDER BY id DESC LIMIT 49;");    
+if ($_GET['type']=="temp" and $_GET['period']=="day"){
+    $querypin=$_GET['pin'];
+    $data = mysql_query("SELECT temperature, timestamp FROM dht_data WHERE pin=$querypin ORDER BY id DESC LIMIT 49;");    
 
 //лепим массив данных. всего 49 значений (0-48)
     $i=48;
-    while ($row = mysqli_fetch_assoc($data)) {
+    while ($row = mysql_fetch_assoc($data)) {
         $temp[$i]=400-$row["temperature"]*10;//координаты по У, температура. Диапазон от 0 до 40 градусов
         $time[$i]=$row["timestamp"];//координаты по Х, время. координата вычисляется дальше
         $i--;
@@ -41,9 +54,9 @@ if ($_GET['type']=="temp24"){
         $houraxis = $houraxis   .'<text class="xtime" x="'.$hour.'" y="399" fill="#777" transform="rotate(-90 '.$hour.',399)">'.date('H:i',$time[$i])."</text>\n"; //%HOURAXIS%
         $dayaxis = $dayaxis     .'<text class="xtime" x="'.$day.'" y="399" fill="#aaa" transform="rotate(-90 '.$day.',399)">'.date('d.m',$time[$i])."</text>\n"; //%DAYAXIS%
     }
-        
+       
     $curdateval = date("H:i:s, d.m.Y",$time[48]); //%CURRENTDOTDATATIME%
-    $template = file_get_contents('./tpl/tempday.txt');
+    $template = file_get_contents('../template/charts/tempday.txt');
     $returner = str_replace("%CURRENTDOTX%",$x,$template);
     $returner = str_replace("%CURRENTDOTY%",$temp[48],$returner);
     $returner = str_replace("%CURRENTDOTVALUE%",$currdotval,$returner);
@@ -55,18 +68,21 @@ if ($_GET['type']=="temp24"){
     $returner = str_replace("%POLYPOINTS%",$polypoints,$returner);
 
     echo $returner;
+exit(0);
 }
 
 
 ///////////////////////////////////////////
 /////////////СУТОЧНАЯ ВЛАЖНОСТЬ////////////
 ///////////////////////////////////////////
-if ($_GET['type']=="hmdt24"){
-    $data = mysqli_query($db, "SELECT humidity, timestamp FROM dht_data WHERE pin=3 ORDER BY id DESC LIMIT 49;");    
+if ($_GET['type']=="hmdt" and $_GET['period']=="day"){
 
+    $querypin=$_GET['pin'];
+    $data = mysql_query("SELECT humidity, timestamp FROM dht_data WHERE pin=$querypin ORDER BY id DESC LIMIT 49;");    
+    
 //лепим массив данных. всего 49 значений (0-48)
     $i=48;
-    while ($row = mysqli_fetch_assoc($data)) {
+    while ($row = mysql_fetch_assoc($data)) {
         $hmdt[$i]=400-$row["humidity"]*4;//координаты по У, температура. Диапазон от 0 до 40 градусов
         $time[$i]=$row["timestamp"];//координаты по Х, время. координата вычисляется дальше
         $i--;
@@ -91,7 +107,7 @@ if ($_GET['type']=="hmdt24"){
     }
         
     $curdateval = date("H:i:s, d.m.Y",$time[48]); //%CURRENTDOTDATATIME%
-    $template = file_get_contents('./tpl/hmdtday.txt');
+    $template = file_get_contents('../template/charts/hmdtday.txt');
     $returner = str_replace("%CURRENTDOTX%",$x,$template);
     $returner = str_replace("%CURRENTDOTY%",$hmdt[48],$returner);
     $returner = str_replace("%CURRENTDOTVALUE%",$currdotval,$returner);
@@ -113,10 +129,11 @@ if ($_GET['type']=="hmdt24"){
 ///////////////////////////////////////////
 
 
-if ($_GET['type']=="temp7"){
-    $data = mysqli_query($db, "SELECT temperature, timestamp FROM dht_data WHERE pin=3 ORDER BY id DESC LIMIT 337;");    
+if ($_GET['type']=="temp" and $_GET['period']=="week"){
+    $querypin=$_GET['pin'];
+    $data = mysql_query("SELECT temperature, timestamp FROM dht_data WHERE pin=$querypin ORDER BY id DESC LIMIT 337;");    
     $i=336;
-    while ($row = mysqli_fetch_assoc($data)) {
+    while ($row = mysql_fetch_assoc($data)) {
         $temp[$i]=400-$row["temperature"]*10;//координаты по У, температура. Диапазон от 0 до 40 градусов
         $time[$i]=$row["timestamp"];//координаты по Х, время. координата вычисляется дальше
         $i--;
@@ -144,7 +161,7 @@ if ($_GET['type']=="temp7"){
 
 
     $curdateval = date("H:i:s, d.m.Y",$time[336]); //%CURRENTDOTDATATIME%
-    $template = file_get_contents('./tpl/tempweek.txt');
+    $template = file_get_contents('../template/charts/tempweek.txt');
     $returner = str_replace("%CURRENTDOTX%",$x,$template);
     $returner = str_replace("%CURRENTDOTY%",$temp[336],$returner);
     $returner = str_replace("%CURRENTDOTVALUE%",$currdotval,$returner);
@@ -166,12 +183,13 @@ if ($_GET['type']=="temp7"){
 ///////////////////////////////////////////
 
 
-if ($_GET['type']=="hmdt7"){
-    $data = mysqli_query($db, "SELECT humidity, timestamp FROM dht_data WHERE pin=3 ORDER BY id DESC LIMIT 337;");    
+if ($_GET['type']=="hmdt" and $_GET['period']=="week"){
+    $querypin=$_GET['pin'];
+    $data = mysql_query("SELECT humidity, timestamp FROM dht_data WHERE pin=$querypin ORDER BY id DESC LIMIT 337;");    
 
 //лепим массив данных. всего 49 значений (0-48)
     $i=336;
-    while ($row = mysqli_fetch_assoc($data)) {
+    while ($row = mysql_fetch_assoc($data)) {
         $hmdt[$i]=400-$row["humidity"]*4;//координаты по У, температура. Диапазон от 0 до 40 градусов
         $time[$i]=$row["timestamp"];//координаты по Х, время. координата вычисляется дальше
         $i--;
@@ -196,7 +214,7 @@ if ($_GET['type']=="hmdt7"){
     }
         
     $curdateval = date("H:i:s, d.m.Y",$time[336]); //%CURRENTDOTDATATIME%
-    $template = file_get_contents('./tpl/hmdtweek.txt');
+    $template = file_get_contents('../template/charts/hmdtweek.txt');
     $returner = str_replace("%CURRENTDOTX%",$x,$template);
     $returner = str_replace("%CURRENTDOTY%",$hmdt[336],$returner);
     $returner = str_replace("%CURRENTDOTVALUE%",$currdotval,$returner);
